b167e0c33cfc139c248b5cd54da9ad6e5cebb7fe
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-02-27 19:48:32 -0600

    adding worthit symbol

--- store_item.rb
+++ store_item.rb
@@ -1,3 +1,7 @@
-item1 = {:color => "red", :price => "5 bucks", :fragility => "is not"}
-item2 = {:color => "blue", :price => "6 bucks", :fragility => "is"}
-item3 = {:color => "white", :price => "2 bucks", :fragility => "is not"}
+item1 = {:color => "red", :price => "5 bucks", :fragility => "is not", :worth_it? => "probably not"}
+item2 = {:color => "blue", :price => "6 bucks", :fragility => "is", :worth_it? => "maybe"}
+item3 = {:color => "white", :price => "2 bucks", :fragility => "is not", :worth_it? => "hell yeah it is"}
+
+
+
+
