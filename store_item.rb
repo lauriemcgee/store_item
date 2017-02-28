07c86aab7594b4459b155451afe3db9ffbf017ad
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-02-27 19:42:44 -0600

    added hashes

---
+++ store_item.rb
@@ -0,0 +1,3 @@
+item1 = {:color => "red", :price => "5 bucks", :fragility => "is not"}
+item2 = {:color => "blue", :price => "6 bucks", :fragility => "is"}
+item3 = {:color => "white", :price => "2 bucks", :fragility => "is not"}
