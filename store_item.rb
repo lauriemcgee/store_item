4b48e2ee157132534e26f6481bf04586dcc69aed
lauriemcgee <livingoncoffeeandflowers@gmail.com>
2017-02-28 19:59:33 -0600

    shelf_life method added

--- store_item.rb
+++ store_item.rb
@@ -30,3 +30,39 @@ puts item1.fragility
 puts item1.info
 item1.worth_it = "probably"
 puts item1.worth_it
+
+class Store
+  attr_reader :color, :price, :fragility, :worth_it
+  attr_writer :worth_it
+  def initialize(item_input)
+    @color = item_input[:color]
+    @price = item_input[:price]
+    @fragility = item_input[:fragility]
+    @worth_it = item_input[:worth_it]
+  end
+  def info
+    puts "This here #{@color} item is #{@price} and is #{@worth_it} worth it."
+  end
+end
+
+item2 = Store.new({color: "Blue", price: "5 bucks", fragility: "nope", worth_it: "maybe"})
+puts item2.info
+
+# Class called food that inherits from Store class and has a shelf_life property
+
+class Food < Store
+  attr_reader :shelf_life
+  def initialize(item_input)
+    super
+    @shelf_life = item_input[:shelf_life]
+  end
+end
+
+food = Food.new({color: "Blue", price: "5 bucks", fragility: "nope", worth_it: "maybe", shelf_life: "not very long"})
+
+p food.shelf_life
+
+
+
+
+
